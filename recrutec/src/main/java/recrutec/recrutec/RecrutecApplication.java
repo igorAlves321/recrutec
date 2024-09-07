84c2ab9015bcc926cbcba5bdf3bb65f9581a8557
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-09-07 02:58:08 -0300

    subindo projeto inicial.

---
+++ recrutec/src/main/java/recrutec/recrutec/RecrutecApplication.java
@@ -0,0 +1,13 @@
+package recrutec.recrutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutecApplication.class, args);
+	}
+
+}
---
+++ recrutec/src/test/java/recrutec/recrutec/RecrutecApplicationTests.java
@@ -0,0 +1,13 @@
+package recrutec.recrutec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
