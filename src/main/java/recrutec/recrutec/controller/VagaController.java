c5f0f2534916473fdd88df172037b6e0ad9f809f
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-09-11 01:21:54 -0300

    agora o id do usuário logado é passado, para que a vaga seja vinculada ao recrutador logado.

--- src/main/java/recrutec/recrutec/controller/VagaController.java
+++ src/main/java/recrutec/recrutec/controller/VagaController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import recrutec.recrutec.model.Vaga;
+import recrutec.recrutec.model.Recrutador;
 import recrutec.recrutec.service.VagaService;
+import recrutec.recrutec.service.RecrutadorService;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,12 +19,17 @@ public class VagaController {
     @Autowired
     private VagaService vagaService;
 
+    @Autowired
+    private RecrutadorService recrutadorService;
+
+    // Endpoint para listar todas as vagas
     @GetMapping
     public ResponseEntity<List<Vaga>> listarVagas() {
         List<Vaga> vagas = vagaService.listarVagas();
         return new ResponseEntity<>(vagas, HttpStatus.OK);
     }
 
+    // Endpoint para buscar uma vaga por ID
     @GetMapping("/{id}")
     public ResponseEntity<Vaga> buscarVagaPorId(@PathVariable Long id) {
         Optional<Vaga> vaga = vagaService.buscarVagaPorId(id);
@@ -32,12 +39,21 @@ public class VagaController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    // Endpoint para salvar uma nova vaga, associando ao recrutador
     @PostMapping
-    public ResponseEntity<Vaga> salvarVaga(@RequestBody Vaga vaga) {
-        Vaga novaVaga = vagaService.salvarVaga(vaga);
-        return new ResponseEntity<>(novaVaga, HttpStatus.CREATED);
+    public ResponseEntity<Vaga> salvarVaga(@RequestBody Vaga vaga, @RequestParam Long recrutadorId) {
+        Optional<Recrutador> recrutador = recrutadorService.buscarRecrutadorPorId(recrutadorId);
+        
+        if (recrutador.isPresent()) {
+            vaga.setRecrutador(recrutador.get());  // Associa o recrutador à vaga
+            Vaga novaVaga = vagaService.salvarVaga(vaga);
+            return new ResponseEntity<>(novaVaga, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);  // Recrutador não encontrado
+        }
     }
 
+    // Endpoint para deletar uma vaga por ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarVaga(@PathVariable Long id) {
         Optional<Vaga> vaga = vagaService.buscarVagaPorId(id);
